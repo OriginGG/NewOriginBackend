0fe72494b7ebf76432615ca422fe4cd4b7d7a8b1
Brandon Simon <brandon.n.simon@gmail.com>
2020-11-24 22:20:18 -0600

    changed traefik config to contain dummy domains pending database rebuild

--- src/customDomainConfig/index.ts
+++ src/customDomainConfig/index.ts
@@ -10,8 +10,8 @@ const client = new Client()
     await client.connect()
 
     app.get('/', async (_, res) => {
-        const { rows: hosts } = await client.query('select host from domain_registration as a inner join organisation_account as b on a.token = b.domain_uuid_token')
-        res.json(makeTraefikConfig(hosts.map(({host}) => host)))
+        // const { rows: hosts } = await client.query('select host from domain_registration as a inner join organisation_account as b on a.token = b.domain_uuid_token')
+        res.json(makeTraefikConfig(['origintestcustomdomain1', 'origintestcustomdomain2']))
     })
 
     app.listen(process.env.CUSTOM_DOMAIN_CONFIG_PORT || 80, () => console.log(`traefikConfig started`))

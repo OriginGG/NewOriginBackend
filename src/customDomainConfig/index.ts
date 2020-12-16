907213647dc9ab1ba61aff3416f50c432ee08d23
Brandon Simon <brandon.n.simon@gmail.com>
2020-12-16 00:23:30 -0600

    removed fake custom domains because you can test a theme by putting in domain=<domain> as a query parameter on the theme instead

--- src/customDomainConfig/index.ts
+++ src/customDomainConfig/index.ts
@@ -12,7 +12,7 @@ const client = new Client()
 
     app.get('/', async (_, res) => {
         // const { rows: hosts } = await client.query('select host from domain_registration as a inner join organisation_account as b on a.token = b.domain_uuid_token')
-        res.json(makeTraefikConfig(['origintestcustomdomain1', 'origintestcustomdomain2']))
+        res.json(makeTraefikConfig([]))
     })
 
     app.listen(process.env.CUSTOM_DOMAIN_CONFIG_PORT || 80, () => console.log(`traefikConfig started`))
